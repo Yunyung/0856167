4af6a09535d16acf4a80a860b30545e147d92eb0
Yunyung <yungyung7654321@gmail.com>
2021-03-22 05:05:57 +0800

    feat: All Things work

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -27,22 +27,22 @@ public class PriorityQueueTest {
     }
 
     public static Stream<Arguments> getParameters() {
-        // return Stream.of(
-        //         Arguments.of(new int[] {2, 4, 3}, new int[] {2, 3, 4}),
-        //         Arguments.of(new int[] {4, 6, 2, 0}, new int[] {0, 2, 4, 6}),
-        //         Arguments.of(new int[] {-8, 6, 0, -4}, new int[] {-8, -4, 0, 6}),
-        //         Arguments.of(new int[] {7, 9, 5, 8, -1}, new int[] {-1, 5, 7, 8, 9}),
-        //         Arguments.of(new int[] {3, 6, 9, -2}, new int[] {-2, 3, 6, 9})
-        // );
-        
-        // Failure version to test CI(add 1 to value of zero index )
         return Stream.of(
-                Arguments.of(new int[] {2, 4, 3}, new int[] {3, 3, 4}),
-                Arguments.of(new int[] {4, 6, 2, 0}, new int[] {1, 2, 4, 6}),
-                Arguments.of(new int[] {-8, 6, 0, -4}, new int[] {-7, -4, 0, 6}),
-                Arguments.of(new int[] {7, 9, 5, 8, -1}, new int[] {0, 5, 7, 8, 9}),
-                Arguments.of(new int[] {3, 6, 9, -2}, new int[] {-1, 3, 6, 9})
+                Arguments.of(new int[] {2, 4, 3}, new int[] {2, 3, 4}),
+                Arguments.of(new int[] {4, 6, 2, 0}, new int[] {0, 2, 4, 6}),
+                Arguments.of(new int[] {-8, 6, 0, -4}, new int[] {-8, -4, 0, 6}),
+                Arguments.of(new int[] {7, 9, 5, 8, -1}, new int[] {-1, 5, 7, 8, 9}),
+                Arguments.of(new int[] {3, 6, 9, -2}, new int[] {-2, 3, 6, 9})
         );
+        
+        /* Failure version to test CI(add 1 to value of zero index) */
+        // return Stream.of(
+        //         Arguments.of(new int[] {2, 4, 3}, new int[] {3, 3, 4}),
+        //         Arguments.of(new int[] {4, 6, 2, 0}, new int[] {1, 2, 4, 6}),
+        //         Arguments.of(new int[] {-8, 6, 0, -4}, new int[] {-7, -4, 0, 6}),
+        //         Arguments.of(new int[] {7, 9, 5, 8, -1}, new int[] {0, 5, 7, 8, 9}),
+        //         Arguments.of(new int[] {3, 6, 9, -2}, new int[] {-1, 3, 6, 9})
+        // );
     }
 
     @Test
